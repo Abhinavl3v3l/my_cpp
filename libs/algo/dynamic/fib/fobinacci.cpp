aab51102e3e780a28b3af314e1a2f5e0bb61488c
Abhinavl3v3l <rabhinavcs@gmail.com>
2021-08-12 07:44:41 +0530

    Update 12 August

---
+++ libs/algo/dynamic/fib/fobinacci.cpp
@@ -0,0 +1,30 @@
+#define ZERO 0
+#define ONE 1
+
+namespace algo{
+
+/* 
+ 0 1 1 2 3 5 8 13 21 34 55 89 
+ 0 1 2 3 4 5 6  7  8  9 10 11 
+ */
+/**
+ * @brief 
+ * 	Brute force fibonacci
+ */
+unsigned int  fibonacci_brute(unsigned int place){
+	unsigned int value = ZERO + ONE;
+	if ( place == 0 ) return 0;
+	if ( place == 1 ) return 1;
+	else return value + 
+}
+
+
+/**
+ * @brief 
+ *  Optimized Fibonacci
+ */
+void  fibonacci_optimized(unsigned int place){
+	return;
+}
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <iterator>
-// #include "libs/helloworld.hpp"
-// #include "libs/enum_test.hpp"
-// #include "libs/MaestroDevice.hpp"
-// unsigned int microsecond = 1000000;
-
-#include<unistd.h>
 
 int main(int argc, char const *argv[])
 {
 
-std::vector<std::string> links;
-for (int i = 0;i< 13;i++) {links.push_back(std::to_string(i));}
 
 
-
-std::cout << links(0);
-
-  return 0;
+return 0;
 }
