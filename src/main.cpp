6d334293e549bca02a49148c9a83571304f914e9
Abhinavl3v3l <rabhinavcs@gmail.com>
2021-08-31 06:35:35 +0530

    Update 31 August 2021

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,28 @@
 #include <vector>
 #include <string>
 #include <typeinfo>
-
+#include <numeric>
 #include <algorithm>
+#include <typeinfo>
+#include <bits/stdc++.h>
+
+
+
 int main(int argc, char const *argv[])
-{
-std::vector<int> v(10, 2);
-      
-    // illustrate all_of
-	const auto in_range = [](int i){ return i%2 == 0;};
-    if (std::all_of(v.cbegin(), v.cend(),in_range)) 
-    {
-        std::cout << "All numbers are even\n";
-    }
+{	
+	std::vector<std::string> key = {"a","b","c"};
+	std::vector<float> value = {1,2,3};
+	std::map<std::string,float> new_map;
+	for(auto k:key){
+		std::cout  << k << " " << "\n";
+	}
+
+	std::transform(std::begin(key), std::end(key)
+             , std::begin(value)
+             , std::inserter(new_map, std::begin(new_map))
+             , [] (std::string key, float value) { return std::make_pair(key, value); });
+
+	std::map::iterator mi;
+	for()
 	return 0;
 }
